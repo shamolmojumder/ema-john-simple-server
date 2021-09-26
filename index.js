6ed2adc4018874c7ed94c55e65cb79649cb37b7d
shamolmojumder <shamolmojumder14@gmail.com>
2021-09-27 01:24:01 +0600

    48-4 load data from the server

--- index.js
+++ index.js
@@ -24,14 +24,29 @@ app.get('/',(req,res)=>{
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
-  app.post('/addProduct',(req,res)=>{
-    const products=req.body;
-    productsCollection.insertMany(products)
-    .then(result=>{
-      console.log(result.insertedCount);
-      res.send(result.insertedCount);
+    app.post('/addProduct',(req,res)=>{
+      const products=req.body;
+      productsCollection.insertMany(products)
+      .then(result=>{
+        console.log(result.insertedCount);
+        res.send(result.insertedCount);
+      })
     })
-  })
+
+    app.get('/products',(req,res)=>{
+      productsCollection.find({})
+      .toArray((err,documents)=>{
+        res.send(documents)
+      })
+    })
+
+    app.get('/product/:key',(req,res)=>{
+      productsCollection.find({key:req.params.key})
+      .toArray((err,documents)=>{
+        res.send(documents[0])
+      })
+    })
+
 });
 
 
